875215616705612f35b275e85b64227225da1aa9
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2024-06-26 01:52:39 -0600

    aws

--- shotsbyaxcel/src/data/videos.tsx
+++ shotsbyaxcel/src/data/videos.tsx
@@ -35,8 +35,8 @@
 
 
 export const Videos = [
-    { title: "Asake x Axcel", src: "/videos/video1.mp4" },
-    { title: "New Balance x Axcel", src: "/videos/video2.mp4" },
-    { title: "Usports x Axcel", src: "/videos/video3.mp4" },
-    { title: "Montreal x Axcel", src: "/videos/video4.mp4" },
+    { title: "Asake x Axcel", src: "https://shotsbyaxcel.s3.us-east-2.amazonaws.com/video1.mp4" },
+    { title: "New Balance x Axcel", src: "https://shotsbyaxcel.s3.us-east-2.amazonaws.com/video2.mp4" },
+    { title: "Usports x Axcel", src: "https://shotsbyaxcel.s3.us-east-2.amazonaws.com/video3.mp4" },
+    { title: "Montreal x Axcel", src: "https://shotsbyaxcel.s3.us-east-2.amazonaws.com/video4.mp4" },
   ];
