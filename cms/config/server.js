7ae56e786fdbee59b5fade4233b9ec8d4d7e305f
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2024-06-25 23:13:38 -0600

    cms

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,8 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://shotsbyaxcel-cms.vercel.app'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL', 'https://shotsbyaxcel-cms.vercel.app'),
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
